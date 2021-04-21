71c16cf3caaf22e70f6b69ad1ecc0a26d9562274
Максим <goshasnegir@icloud.com>
2021-04-21 12:11:51 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 exec gunicorn Oceanstart.wsgi:application --bind 0.0.0.0:8000 --reload
